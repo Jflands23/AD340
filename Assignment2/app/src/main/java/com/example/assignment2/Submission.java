4644801952ab0a05fc90323e11fe09430cadfea1
Jflands23 <75229373+Jflands23@users.noreply.github.com>
2021-04-21 21:35:32 -0700

    Updated assignment 2
    Added a form and intent to change from one activity to the other. Still trying to do the validation and taking information from the form to the other activity

--- Assignment2/app/src/main/java/com/example/assignment2/Submission.java
+++ Assignment2/app/src/main/java/com/example/assignment2/Submission.java
@@ -1,22 +1,15 @@
 package com.example.assignment2;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
-
+public class Submission extends AppCompatActivity{
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.submission);
     }
 
-
-    public void goToSubmission(View view) {
-        Intent intent = new Intent(MainActivity.this, Submission.class);
-        startActivity(intent);
-    }
 }
