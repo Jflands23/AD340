52aa960d60f61ec4c53d0845f30f33e1c9a47b6c
Jflands23 <75229373+Jflands23@users.noreply.github.com>
2021-04-26 20:49:14 -0700

    Week 3 circle CI

--- Assignment2/app/src/main/java/com/example/assignment2/Submission.java
+++ Assignment2/app/src/main/java/com/example/assignment2/Submission.java
@@ -3,13 +3,34 @@ package com.example.assignment2;
 import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
+import android.widget.TextView;;
 
 public class Submission extends AppCompatActivity{
+    TextView textView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.submission);
+        textView = findViewById(R.id.submission);
+
+        StringBuilder msg = new StringBuilder("Thanks for Signing Up ");
+
+        Intent intent = getIntent();
+        Bundle b = intent.getExtras();
+
+        String name = "Example Name";
+
+        if(b != null){
+            if(b.containsKey(Constants.KEY_NAME)) {
+                name = b.getString(Constants.KEY_NAME);
+                msg.append(name);
+            }
+        }
+
+        textView.setText(msg);
+
     }
+
 
 }
