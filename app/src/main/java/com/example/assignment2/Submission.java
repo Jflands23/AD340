36ea21785f1164cad83de26de9c28e161fce8129
Jflands23 <75229373+Jflands23@users.noreply.github.com>
2021-05-04 19:34:04 -0700

    Assignment 2 and Assignment 3 work

--- app/src/main/java/com/example/assignment2/Submission.java
+++ app/src/main/java/com/example/assignment2/Submission.java
@@ -1,13 +1,18 @@
 package com.example.assignment2;
 
-import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.widget.TextView;;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+;
 
 public class Submission extends AppCompatActivity{
     TextView textView;
     TextView bioView;
+    TextView nameView;
+    TextView jobView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,7 +20,8 @@ public class Submission extends AppCompatActivity{
         setContentView(R.layout.submission);
         textView = findViewById(R.id.submission);
         bioView = findViewById(R.id.bio);
-
+        nameView = findViewById(R.id.name);
+        jobView = findViewById(R.id.job);
         StringBuilder msg = new StringBuilder("Thanks for Signing Up ");
 
         Intent intent = getIntent();
@@ -25,12 +31,18 @@ public class Submission extends AppCompatActivity{
         String bio = "";
 
         if(b != null){
-            if(b.containsKey(Constants.KEY_NAME)) {
-                name = b.getString(Constants.KEY_NAME);
+            if(b.containsKey(Constants.KEY_USER)) {
+                name = b.getString(Constants.KEY_USER);
                 msg.append(name);
             }
             if(b.containsKey((Constants.KEY_BIO))) {
                 bioView.setText(b.getString(Constants.KEY_BIO));
+            }
+            if(b.containsKey((Constants.KEY_NAME))) {
+               nameView.setText(b.getString(Constants.KEY_NAME));
+            }
+            if(b.containsKey((Constants.KEY_JOB))) {
+                jobView.setText(b.getString(Constants.KEY_JOB));
             }
         }
 
