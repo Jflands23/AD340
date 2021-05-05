76afead0bd72970e51eef4fc8182878ba9760dbd
Jflands23 <75229373+Jflands23@users.noreply.github.com>
2021-05-04 22:14:53 -0700

    Assignment 4 Added profile fragment

---
+++ app/src/main/java/com/example/assignment2/ProfileFragment.java
@@ -0,0 +1,47 @@
+package com.example.assignment2;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+public class ProfileFragment extends Fragment {
+    private Submission.Attachment attachment;
+    TextView textView;
+    TextView bioView;
+    TextView nameView;
+    TextView jobView;
+
+    @Override
+    public View onCreateView(
+            @NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        // Inflate the layout for this fragment with the ProfileFragment
+        View view = inflater.inflate(R.layout.fragment_profile, container, false);
+
+        textView = view.findViewById(R.id.submission);
+        bioView = view.findViewById(R.id.bio);
+        nameView = view.findViewById(R.id.name);
+        jobView = view.findViewById(R.id.job);
+
+        textView.setText(this.attachment.uname);
+        bioView.setText(this.attachment.bio);
+        nameView.setText(this.attachment.name);
+        jobView.setText(this.attachment.job);
+
+        return view;
+    }
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
+    }
+
+    void setAttachment(Submission.Attachment attach) {
+        this.attachment = attach;
+    }
+}
+
