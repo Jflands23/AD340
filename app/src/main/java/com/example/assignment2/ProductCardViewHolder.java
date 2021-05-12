f8e4ed03f9cd5e61a1406420e2a1643eef83c472
Jflands23 <75229373+Jflands23@users.noreply.github.com>
2021-05-11 22:20:38 -0700

    Assignment 5 got some cards to show up on matches tab

---
+++ app/src/main/java/com/example/assignment2/ProductCardViewHolder.java
@@ -0,0 +1,14 @@
+package com.example.assignment2;
+
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class ProductCardViewHolder extends RecyclerView.ViewHolder {
+
+    public ProductCardViewHolder(@NonNull View itemView) {
+        super(itemView);
+        // TODO: Find and store views from itemView
+    }
+}
